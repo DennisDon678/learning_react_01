dc6670d2cef5aed6ca8b496c63930acdf494d918
Dennisdon678 <dennisdon678@gmail.com>
2024-02-18 18:04:30 -0500

    Done

--- background_changer/src/App.jsx
+++ background_changer/src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
 
+  const updateColor = (change)=>{
+    setColor(change)
+  }
+  
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+        <div className="buttons">
+          <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-3" onClick={()=>updateColor('red')}>
+            Red
+          </button>
+          <button className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-3" onClick={()=>updateColor('green')}>
+            Green
+          </button>
+          <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-3" onClick={()=>updateColor('black')}>
+            Black
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
